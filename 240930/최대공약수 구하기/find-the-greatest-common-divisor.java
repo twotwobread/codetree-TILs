bc1362bf0685a5c05c9908dd946d5505961944d3
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2024-09-29 17:25:49 +0200

    Title: 최대공약수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240930/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 240930/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -2,13 +2,11 @@ import java.io.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 여기에 코드를 작성해주세요.\
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
         int r = n;
         while(m % r != 0){
@@ -16,10 +14,8 @@ public class Main {
             r = m % n;
             m = temp;
         }
-        bw.write(r + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(r);
+
         //18 % 12 = 6
         //12 % 6 = 0
 
